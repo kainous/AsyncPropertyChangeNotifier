2981bdb797db342fe75ce2a1771de222cc629311
Nicholas Cooper <kainous@gmail.com>
2016-02-23 15:54:27 -0600

    Completed the AsyncList without testing

--- AsyncPropertyChangeNotifier/AsyncPropertyChangeNotifier/AsyncList.cs
+++ AsyncPropertyChangeNotifier/AsyncPropertyChangeNotifier/AsyncList.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,41 +16,73 @@ namespace System.ComponentModel
          , INotifyCollectionChanged
          , INotifyPropertyChanged
     {
+        #region Private members
+
         private readonly List<T> _items = new List<T>();
         private readonly AsyncReaderWriterLock _lock = new AsyncReaderWriterLock();
+
+        #endregion
+
+        #region Events
 
         public event NotifyCollectionChangedEventHandler CollectionChanged;
         public event PropertyChangedEventHandler PropertyChanged;
 
-        #region GetAt
+        #endregion
 
-        protected virtual T GetAtCore(int index)
-        { return _items[index]; }
+        #region Utility methods
 
-        public async Task<T> GetAtAsync(Task<int> index, CancellationToken token)
-        {
-            using (await _lock.ReaderLockAsync(token))
-            { return GetAtCore(await index); }
-        }
-
-        public Task<T> GetAtAsync(Task<int> index)
-        { return GetAtAsync(index, CancellationToken.None); }
-
-        public Task<T> GetAtAsync(int index)
-        { return GetAtAsync(Task.FromResult(index), CancellationToken.None); }
-
-        public T GetAt(int index)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private TIn ReadLock<TIn>(Func<TIn> func)
         {
             using (_lock.ReaderLock())
-            { return GetAtCore(index); }
+            { return func(); }
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private TOut ReadLock<TIn, TOut>(Func<TIn, TOut> func, TIn parameter)
+        {
+            using (_lock.ReaderLock())
+            { return func(parameter); }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private async Task<TIn> ReadLock<TIn>(Func<TIn> func, CancellationToken token)
+        {
+            using (await _lock.ReaderLockAsync(token))
+            { return func(); }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private async Task<TOut> ReadLock<TIn, TOut>(
+            Func<TIn, TOut> func
+          , Task<TIn> input
+          , CancellationToken token)
+        {
+            var newInput = await input;
+            using (await _lock.ReaderLockAsync(token))
+            { return func(newInput); }
+        }
+
+        #endregion
+
+        #region GetAt
+
+        protected virtual T GetAtCore(int index) => _items[index];
+
+        public Task<T> GetAtAsync(Task<int> index, CancellationToken token) => ReadLock(GetAtCore, index, token);
+
+        public Task<T> GetAtAsync(Task<int> index) => GetAtAsync(index, CancellationToken.None);
+
+        public Task<T> GetAtAsync(int index) => GetAtAsync(Task.FromResult(index), CancellationToken.None);
+
+        public T GetAt(int index) => ReadLock(GetAtCore, index);
 
         #endregion
 
         #region SetAt
 
-        protected virtual void SetAtCore(int index, T value)
-        { _items[index] = value; }
+        protected virtual void SetAtCore(int index, T value) => _items[index] = value;
 
         public async Task SetAtAsync(Task<int> index, Task<T> value, CancellationToken token)
         {
@@ -61,14 +95,11 @@ namespace System.ComponentModel
             CollectionChanged.Replace(this, innerIndex, innerValue);
         }
 
-        public Task SetAtAsync(Task<int> index, Task<T> value)
-        { return SetAtAsync(index, value, CancellationToken.None); }
+        public Task SetAtAsync(Task<int> index, Task<T> value) => SetAtAsync(index, value, CancellationToken.None);
 
-        public Task SetAtAsync(int index, Task<T> value)
-        { return SetAtAsync(Task.FromResult(index), value); }
+        public Task SetAtAsync(int index, Task<T> value) => SetAtAsync(Task.FromResult(index), value);
 
-        public Task SetAtAsync(int index, T value)
-        { return SetAtAsync(Task.FromResult(index), Task.FromResult(value)); }
+        public Task SetAtAsync(int index, T value) => SetAtAsync(Task.FromResult(index), Task.FromResult(value));
 
         public void SetAt(int index, T value)
         {
@@ -86,17 +117,12 @@ namespace System.ComponentModel
             set { SetAt(index, value); }
         }
 
-        protected virtual long GetCountCore()
-        { return _items.Count; }
+        protected virtual long GetCountCore() => _items.Count;
 
-        public int Count
-        {
-            get
-            {
-                using (_lock.ReaderLock())
-                { return (int)GetCountCore(); }
-            }
-        }
+
+
+        public int Count => ReadLock(() => (int)GetCountCore());
+
         #endregion
 
         #region Explicit ICollection members
@@ -139,21 +165,27 @@ namespace System.ComponentModel
             }
         }
 
-        public async Task<int> AddAsync(Task<IEnumerable<T>> items)
+        public Task InsertAsync(Task<int> index, Task<IEnumerable<T>> items)
+        { return InsertAsync(index, items, CancellationToken.None); }
+
+        public async Task<int> AddAsync(Task<IEnumerable<T>> items, CancellationToken token)
         {
             //Must not await inside a lock
             var newItems = await items;
             if (newItems != null)
             {
-                using (await _lock.UpgradeableReaderLockAsync())
+                using (await _lock.UpgradeableReaderLockAsync(token))
                 {
                     var index = _items.Count;
-                    await InsertAsync(Task.FromResult(index), items);
+                    await InsertAsync(Task.FromResult(index), items, token);
                     return index;
                 }
             }
             return -1;
         }
+
+        public Task<int> AddAsync(Task<IEnumerable<T>> items)
+        { return AddAsync(items, CancellationToken.None); }
 
         public Task InsertAsync(int index, Task<IEnumerable<T>> items)
         { return InsertAsync(Task.FromResult(index), items); }
@@ -189,59 +221,244 @@ namespace System.ComponentModel
             }
         }
 
+        void IList<T>.Insert(int index, T item)
+        { Insert(index, item); }
+
         #endregion
 
-        public Task ClearAsync()
+        #region Clear logic
+        protected virtual void ClearCore()
+        { _items.Clear(); }
+
+        public async Task ClearAsync(CancellationToken token)
         {
-            using (await _lock.WriterLockAsync())
-            { }
+            using (await _lock.WriterLockAsync(token))
+            { ClearCore(); }
+
+            CollectionChanged.Reset(this);
+            PropertyChanged?.FireAndForget(this, nameof(Count));
         }
 
-
+        public Task ClearAsync()
+        { return ClearAsync(CancellationToken.None); }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            using (_lock.WriterLock())
+            { ClearCore(); }
+
+            CollectionChanged.Reset(this);
+            PropertyChanged?.FireAndForget(this, nameof(Count));
         }
 
-        public bool Contains(T item)
+        #endregion
+
+        #region CopyTo logic
+
+        public async Task<IReadOnlyList<T>> AsReadonlyCopy()
         {
-            throw new NotImplementedException();
+            T[] copy;
+            using (await _lock.ReaderLockAsync())
+            {
+                copy = new T[_items.Count];
+                _items.CopyTo(copy);
+            }
+
+            return copy.ToList().AsReadOnly();
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            using (_lock.ReaderLock())
+            { _items.CopyTo(array, arrayIndex); }
         }
 
+        #endregion
+
+        #region Enumerator logic
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
-        }
+            T[] copy;
+            using (_lock.ReaderLock())
+            {
+                copy = new T[_items.Count];
+                CopyTo(copy, 0);
+            }
 
-        public int IndexOf(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Insert(int index, T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveAt(int index)
-        {
-            throw new NotImplementedException();
+            foreach (var item in copy)
+            { yield return item; }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             throw new NotImplementedException();
         }
+
+        #endregion
+
+        #region Searching logic
+
+        public async Task<bool> ContainsAsync(
+            Task<T> item
+          , IEqualityComparer<T> comparer
+          , CancellationToken token)
+        {
+            var newItem = await item;
+            using (await _lock.ReaderLockAsync(token))
+            { return _items.Contains(newItem, comparer); }
+        }
+        public Task<bool> ContainsAsync(Task<T> item, IEqualityComparer<T> comparer)
+        { return ContainsAsync(item, comparer, CancellationToken.None); }
+
+        public async Task<bool> ContainsAsync(Task<T> item)
+        {
+            var newItem = await item;
+            using (await _lock.ReaderLockAsync())
+            { return _items.Contains(newItem); }
+        }
+
+        public Task<bool> ContainsAsync(T item)
+        { return ContainsAsync(Task.FromResult(item)); }
+
+        public bool Contains(T item)
+        {
+            using (_lock.ReaderLock())
+            { return _items.Contains(item); }
+        }
+
+        protected virtual int IndexOfCore(T item)
+        { return _items.IndexOf(item); }
+
+        public async Task<int> IndexOfAsync(Task<T> item, CancellationToken token)
+        {
+            var newItem = await item;
+            using (await _lock.ReaderLockAsync(token))
+            { return IndexOfCore(newItem); }
+        }
+
+        public Task<int> IndexOfAsync(Task<T> item)
+        { return IndexOfAsync(item, CancellationToken.None); }
+
+        public int IndexOf(T item)
+        {
+            using (_lock.ReaderLock())
+            { return IndexOfCore(item); }
+        }
+
+        #endregion
+
+        #region Remove logic
+        private IEnumerable<KeyValuePair<int, T>> GetReversedEnumerable()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            { yield return new KeyValuePair<int, T>(i, GetAt(i)); }
+        }
+
+        protected virtual void RemoveAtCore(int index)
+        { _items.RemoveAt(index); }
+
+        public async Task<IDictionary<int, T>> RemoveWhere(
+            Func<int, T, bool> condition
+          , CancellationToken token)
+        {
+            var result = new Dictionary<int, T>();
+            long itemCount;
+            using (await _lock.WriterLockAsync(token))
+            {
+                itemCount = GetCountCore();
+                foreach (var kvp in GetReversedEnumerable())
+                {
+                    RemoveAtCore(kvp.Key);
+                    result.Add(kvp.Key, kvp.Value);
+                }
+            }
+
+            CollectionChanged.NotifyRemove(this, itemCount, result);
+            PropertyChanged?.FireAndForget(this, nameof(Count));
+            return result;
+        }
+
+        public Task<IDictionary<int, T>> RemoveWhereAsync(
+            Func<T, bool> condition
+          , CancellationToken token) => RemoveWhere((_, item) => condition(item), token);
+
+        public Task<IDictionary<int, T>> RemoveWhereAsync(Func<T, bool> condition)
+            => RemoveWhereAsync(condition, CancellationToken.None);
+
+
+        public async Task RemoveAtAsync(Task<int> index)
+        {
+            long itemCount;
+            T removedItem;
+            var newIndex = await index;
+            using (await _lock.WriterLockAsync())
+            {
+                itemCount = GetCountCore();
+                removedItem = GetAtCore(newIndex);
+                RemoveAtCore(newIndex);
+            }
+
+            CollectionChanged.NotifyRemove(
+                this
+              , itemCount
+              , new Dictionary<int, T> { { newIndex, removedItem } });
+
+            PropertyChanged.FireAndForget(this, nameof(Count));
+        }
+
+        public Task RemoveAtAsync(int index)
+        { return RemoveAtAsync(Task.FromResult(index)); }
+
+        public async Task<int> RemoveAsync(Task<T> item)
+        {
+            var newItem = await item;
+            using (await _lock.UpgradeableReaderLockAsync())
+            {
+                int index = IndexOfCore(newItem);
+                if (index > 0)
+                { await RemoveAtAsync(index); }
+                return index;
+            }
+
+        }
+
+        public Task<int> RemoveAsync(T item)
+        { return RemoveAsync(Task.FromResult(item)); }
+
+        public void RemoveAt(int index)
+        {
+            long itemCount;
+            T removedItem;
+
+            using (_lock.WriterLock())
+            {
+                itemCount = GetCountCore();
+                removedItem = GetAtCore(index);
+                RemoveAtCore(index);
+            }
+
+            CollectionChanged.NotifyRemove(
+                this
+              , itemCount
+              , new Dictionary<int, T> { { index, removedItem } });
+
+            PropertyChanged.FireAndForget(this, nameof(Count));
+        }
+
+        public int Remove(T item)
+        {
+            using (_lock.UpgradeableReaderLock())
+            {
+                int index = IndexOfCore(item);
+                if (index >= 0)
+                { RemoveAt(index); }
+                return index;
+            }
+        }
+
+        bool ICollection<T>.Remove(T item)
+        { return Remove(item) != -1; }
+
+        #endregion
     }
 }
--- AsyncPropertyChangeNotifier/AsyncPropertyChangeNotifier/InternalUtilities.cs
+++ AsyncPropertyChangeNotifier/AsyncPropertyChangeNotifier/InternalUtilities.cs
@@ -75,5 +75,69 @@ namespace System.ComponentModel
                 handler.FireAndForget(me, args);
             }
         }
+
+        public static void Reset(this NotifyCollectionChangedEventHandler handler, object me)
+        {
+            if (handler != null)
+            {
+                var reset = NotifyCollectionChangedAction.Reset;
+                var args = new NotifyCollectionChangedEventArgs(reset);
+                handler.FireAndForget(me, args);
+            }
+        }
+
+        private static IEnumerable<Tuple<int, int>> GetStartEnd<T>(IDictionary<int, T> items)
+        {
+            if (items == null)
+            { yield break; }
+
+            int start = int.MinValue;
+            int end = int.MinValue;
+            foreach (var key in items.Keys.OrderBy(o => o))
+            {
+                if (start == int.MinValue && end == int.MinValue)
+                {
+                    start = key;
+                    end = key;
+                }
+                else if (key - 1 != start)
+                {
+                    yield return Tuple.Create(start, end);
+                    start = key;
+                    end = key;
+                }
+                else
+                { end = key; }
+            }
+
+            if (start == int.MinValue && end == int.MinValue)
+            { yield return Tuple.Create(start, end); }
+        }
+
+        public static void NotifyRemove<T>(this NotifyCollectionChangedEventHandler handler, object me, long itemCount, IDictionary<int, T> removedItems)
+        {
+            if ((handler == null) || (removedItems == null))
+            { return; }
+
+            var total = GetStartEnd(removedItems).ToArray();
+            if (removedItems.Count / total.Length < 5) //20% change
+            { handler.Reset(me); }
+            else
+            {
+                foreach (var t in total)
+                {
+                    int start = t.Item1;
+                    int count = t.Item2 - start;
+                    var enu = Enumerable.Range(start, count);
+
+                    var args = new NotifyCollectionChangedEventArgs(
+                        NotifyCollectionChangedAction.Remove
+                      , enu.Select(o => removedItems[o]).ToList()
+                      , start);
+
+                    handler(me, args);
+                }
+            }
+        }
     }
 }
